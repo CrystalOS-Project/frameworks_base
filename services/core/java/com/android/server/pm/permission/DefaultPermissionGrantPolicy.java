a00d1b1e4651e1206412bac116668a8fdaba2e0d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-06 16:49:17 +0530

    base: Grant suspend permission to Flipendo
    * extreme battery saver from pixel 5
    
    Change-Id: I7f65629aa5d1e390a47c9faf3b42a950811050a1

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -920,6 +920,11 @@ final class DefaultPermissionGrantPolicy {
         grantSystemFixedPermissionsToSystemPackage(pm,
                 getDefaultProviderAuthorityPackage("com.android.providers.media.MediaProvider", userId), userId,
                 STORAGE_PERMISSIONS);
+
+        // Flipendo
+        grantSystemFixedPermissionsToSystemPackage(pm,
+                getDefaultProviderAuthorityPackage("com.google.android.flipendo", userId),
+                userId, SUSPEND_APP_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(PackageManagerWrapper pm,
