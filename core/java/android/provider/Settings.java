8bf72bd0b15c152b8a6c265bf5ad7a30218ed05a
maxwen <max.weninger@gmail.com>
2021-07-07 20:45:51 +0530

    SystemUI: Add hide statusbar on lockscreen [1/2]
    Taken from Omni ROM FWB-
    omnirom/android_frameworks_base@0b833a0
    
    Source - omnirom/android_frameworks_base@0b833a0
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5599,17 +5599,23 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 		
-		/**
+	/**
          * Double tap on lockscreen to sleep
          * @hide
          */
         public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN = "double_tap_sleep_lockscreen";
 		
-		/**
+	/**
          * Use doubletap as doze pulse triggers
          * @hide
          */
         public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
+
+	/**
+         * Hide Statusbar on LockScreen
+         * @hide
+         */
+        public static final String HIDE_LOCKSCREEN_STATUS_BAR = "hide_lockscreen_status_bar";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
