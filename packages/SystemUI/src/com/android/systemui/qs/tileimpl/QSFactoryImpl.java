a0108ca48288428b852c7070726fea65669a205b
jhonboy121 <alfredmathew05@gmail.com>
2021-09-14 20:23:04 +0530

    [SQUASH]: SystemUI: Add refresh rate tile
    base: added refresh rate tile
    
    * config_defaultPeakRefreshRate must be overlayed to enable this tile
    * supports switching between 60Hz (min) , peak refresh rate and auto mode
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    SystemUI: RefreshRateTile: java -> kt
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    SystemUI: RefreshRateTile: fix getting wrong peak refresh rate default value
    
    * also simplified some code while at it
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    SystemUI: RefreshRateTile: improve and support for multiple peak refresh rates
    
    * Logic for properly acquiring multiple peak refresh rates taken from https://github.com/LineageOS/android_packages_apps_Settings/commit/08c1661b72263eec5c5fdf1cc35d85612f5751c1
    * Added device config listener and simplified mode cycling
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Signed-off-by: Alfred Mathew <alfredmathew05@gmail.com>
    
    SystemUI: RefreshRateTile: fix mode cycling
    
    * Makes it work hand-in-hand with setting preferences. This is indeed a better logic as well
    
    Signed-off-by: Alfred Mathew <alfredmathew05@gmail.com>
    
    fixup! SystemUI: RefreshRateTile: improve and support for multiple peak refresh rates
    
    Signed-off-by: Alfred Mathew <alfredmathew05@gmail.com>
    
    SystemUI: RefreshRateTile: Fix mode cycle on some devices
    
    getSupportedModes() return modes sorted by modeId, but cycleToNextMode()
    requires mode to be perfectly sorted by natural order (60, 90, 120, and
    so on).
    The problem is, not every devices got it in natural order, so we have to
    sort it manually.
    
    SystemUI: RefreshRateTile: Fix mode cycle on some devices
    
    getSupportedModes() return modes sorted by modeId, but cycleToNextMode()
    requires mode to be perfectly sorted by natural order (60, 90, 120, and
    so on).
    The problem is, not every devices got it in natural order, so we have to
    sort it manually.
    
    Signed-off-by: minarypenguin <alexfinhart@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: str143 <alexfinhart@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -52,6 +52,7 @@ import com.android.systemui.qs.tiles.OneHandedModeTile;
 import com.android.systemui.qs.tiles.QRCodeScannerTile;
 import com.android.systemui.qs.tiles.QuickAccessWalletTile;
 import com.android.systemui.qs.tiles.ReduceBrightColorsTile;
+import com.android.systemui.qs.tiles.RefreshRateTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
@@ -104,6 +105,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<CompassTile> mCompassTileProvider;
     private final Provider<SleepModeTile> mSleepModeTileProvider;
     private final Provider<HeadsUpTile> mHeadsUpTileProvider;
+    private final Provider<RefreshRateTile> mRefreshRateTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -144,7 +146,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<SmartPixelsTile> smartPixelsTileProvider,
             Provider<CompassTile> compassTileProvider,
             Provider<SleepModeTile> sleepModeTileProvider,
-            Provider<HeadsUpTile> headsUpTileProvider) {
+            Provider<HeadsUpTile> headsUpTileProvider,
+            Provider<RefreshRateTile> refreshRateTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -181,6 +184,7 @@ public class QSFactoryImpl implements QSFactory {
         mCompassTileProvider = compassTileProvider;
         mSleepModeTileProvider = sleepModeTileProvider;
         mHeadsUpTileProvider = headsUpTileProvider;
+        mRefreshRateTileProvider = refreshRateTileProvider;
     }
 
     /** Creates a tile with a type based on {@code tileSpec} */
@@ -262,6 +266,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mSleepModeTileProvider.get();
             case "heads_up":
                 return mHeadsUpTileProvider.get();
+            case "refresh_rate":
+                return mRefreshRateTileProvider.get();
         }
 
         // Custom tiles
