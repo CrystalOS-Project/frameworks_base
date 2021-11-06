094946589a4fc456b6145ca6f3e8f90cdd121133
SagarMakhar <sagarmakhar@gmail.com>
2021-11-06 14:09:41 +0000

    base: Make "Require unlocking to use sensitive QS tiles" optional [1/2]
    Change-Id: I355ef643fc046ce50f40924db130ad212fea5800
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5692,6 +5692,11 @@ public final class Settings {
          */
         @Readable
         public static final String BURN_IN_PROTECTION_INTERVAL = "burn_in_protection_interval";
+		
+	/**
+         * @hide
+         */
+        public static final String USE_TILES_ON_SECURE_KEYGUARD = "use_tiles_on_secure_keyguard";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
