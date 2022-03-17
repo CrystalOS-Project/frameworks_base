3f09c83b9233b65f1868b319949aac770fe26112
spkal01 <kalligeross@gmail.com>
2022-03-17 11:10:12 +0800

    fix-up: base: Introduce Custom settings layout
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Change-Id: Ic7a9fbce12a628a67201c24d8603d8bee4c28af6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5506,13 +5506,30 @@ public final class Settings {
          * @hide
          */
         public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
-		
+
 	/**
          * Settings Dashboard Style
          * @hide
          */
-        public static final String SETTINGS_DASHBOARD_GMS = "settings_dashboard_gms";
+        public static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
 
+	/**
+	 * Settings Base Layout
+	 * @hide
+	 */
+	public static final String ALT_SETTINGS_LAYOUT = "alt_settings_layout";
+
+	/**
+	 * Settings Homepage Layout
+	 * @hide
+	 */
+	public static final String USE_STOCK_LAYOUT = "use_stock_layout";
+
+	/**
+	 * Settings UserCard
+	 * @hide
+	 */
+	public static final String DISABLE_USERCARD = "disable_usercard";
 
         /**
          * Whether the phone vibrates on call connect
