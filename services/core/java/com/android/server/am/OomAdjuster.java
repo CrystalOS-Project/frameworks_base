278864b40d904be1e5ced4ec34efea96c3fdbc1c
minaripenguin37 <alexfinhart@gmail.com>
2022-10-15 07:27:32 +0800

    OOMAdjuster: Fix bootloop/NPE when renderthread boost is enabled
    10-15 07:20:48.327  8808  8860 E AndroidRuntime: *** FATAL EXCEPTION IN SYSTEM PROCESS: android.display
    10-15 07:20:48.327  8808  8860 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'void android.os.PowerManagerInternal.setPowerMode(int, boolean)' on a null object reference
    10-15 07:20:48.327  8808  8860 E AndroidRuntime: 	at com.android.server.am.OomAdjuster.computeOomAdjLSP(OomAdjuster.java:1629)
    10-15 07:20:48.327  8808  8860 E AndroidRuntime: 	at com.android.server.am.OomAdjuster.updateOomAdjInnerLSP(OomAdjuster.java:825)
    10-15 07:20:48.327  8808  8860 E AndroidRuntime: 	at com.android.server.am.OomAdjuster.performUpdateOomAdjLSP(OomAdjuster.java:465)
    10-15 07:20:48.327  8808  8860 E AndroidRuntime: 	at com.android.server.am.OomAdjuster.updateOomAdjLSP(OomAdjuster.java:451)
    10-15 07:20:48.327  8808  8860 E AndroidRuntime: 	at com.android.server.am.OomAdjuster.updateOomAdjLocked(OomAdjuster.java:439)
    10-15 07:20:48.327  8808  8860 E AndroidRuntime: 	at com.android.server.am.ActivityManagerService.updateOomAdjLocked(ActivityManagerService.java:15546)
    10-15 07:20:48.327  8808  8860 E AndroidRuntime: 	at com.android.server.am.ActivityManagerService.trimApplicationsLocked(ActivityManagerService.java:15840)
    10-15 07:20:48.327  8808  8860 E AndroidRuntime: 	at com.android.server.am.ActivityManagerService.trimApplications(ActivityManagerService.java:15792)
    10-15 07:20:48.327  8808  8860 E AndroidRuntime: 	at com.android.server.am.ActivityManagerService.-$$Nest$mtrimApplications(Unknown Source:0)
    10-15 07:20:48.327  8808  8860 E AndroidRuntime: 	at com.android.server.am.ActivityManagerService$LocalService.trimApplications(ActivityManagerService.java:16746)
    10-15 07:20:48.327  8808  8860 E AndroidRuntime: 	at com.android.server.wm.ActivityTaskSupervisor.lambda$activityIdleInternal$2$com-android-server-wm-ActivityTaskSupervisor(ActivityTaskSupervisor.java:1419)
    
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

--- services/core/java/com/android/server/am/OomAdjuster.java
+++ services/core/java/com/android/server/am/OomAdjuster.java
@@ -1580,7 +1580,9 @@ public class OomAdjuster {
             if(mIsTopAppRenderThreadBoostEnabled) {
                 if(mCurRenderThreadTid != app.getRenderThreadTid() && app.getRenderThreadTid() > 0) {
                     mCurRenderThreadTid = app.getRenderThreadTid();
-                    mLocalPowerManager.setPowerMode(Mode.GAME_LOADING, true);
+                    if (mLocalPowerManager != null) {
+                      mLocalPowerManager.setPowerMode(Mode.GAME_LOADING, true);
+                    }
                 }
             }
 
