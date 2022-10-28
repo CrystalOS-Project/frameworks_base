6be3d935d404eb316480b0f40574eb6070afe7cf
EnesSastim <sastimenes@gmail.com>
2022-10-28 08:27:37 +0800

    PixelPropsUtils: Don't spoof euicc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

--- core/java/com/android/internal/crystal/utils/PixelPropsUtils.java
+++ core/java/com/android/internal/crystal/utils/PixelPropsUtils.java
@@ -80,6 +80,7 @@ public class PixelPropsUtils {
             "com.google.android.apps.cameralite",
             "com.google.android.apps.recorder",
             "com.google.android.apps.wearables.maestro.companion",
+            "com.google.android.euicc",
             "com.google.android.apps.youtube.kids",
             "com.google.android.apps.youtube.music",
             "com.google.android.dialer",
