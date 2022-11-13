45e82342d4a4c356dbed2ad176db146d75ef251d
minaripenguin37 <alexfinhart@gmail.com>
2022-11-14 07:19:25 +0800

    Scarlet Services: Introduce optional aggresive idle mode [1/2]
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -3712,7 +3712,15 @@ public class CentralSurfacesImpl extends CoreStartable implements
                     ScarletIdleManager.startAssistantServices();
                     ScarletIdleManager.cacheCleaner(CentralSurfaces.getPackageManagerForUser(mContext, mLockscreenUserManager.getCurrentUserId()));
                 }
+            if (Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                                              Settings.Secure.SCARLET_AGGRESSIVE_MODE, 0,
+                                              mLockscreenUserManager.getCurrentUserId()) == 1) {
+                Settings.Secure.putIntForUser(mContext.getContentResolver(),
+                                              Settings.Secure.SCARLET_AGGRESSIVE_MODE_TRIGGER, 1,
+                                              mLockscreenUserManager.getCurrentUserId());
             }
+         }
+            
         }
 
         @Override
@@ -3747,7 +3755,14 @@ public class CentralSurfacesImpl extends CoreStartable implements
                                               Settings.System.SCARLET_IDLE_ASSISTANT_MANAGER, 1,
                                               mLockscreenUserManager.getCurrentUserId()) == 1) {
                 ScarletIdleManager.stopManager(mContext);
-            }
+              if (Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                                                Settings.Secure.SCARLET_AGGRESSIVE_MODE, 0,
+                                                mLockscreenUserManager.getCurrentUserId()) == 1) {
+                  Settings.Secure.putIntForUser(mContext.getContentResolver(),
+                                                Settings.Secure.SCARLET_AGGRESSIVE_MODE_TRIGGER, 0,
+                                                mLockscreenUserManager.getCurrentUserId());
+             }
+          }
         }
 
         @Override
